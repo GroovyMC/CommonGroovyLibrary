73023c672a9d96f81867ca7d959b4eed21dd22a2
Luke Bemish <lukebemish@gmail.com>
2022-09-01 15:10:22 -0600

    Update CodecTesting.groovy

--- Forge/src/test/groovy/cgltest/CodecTesting.groovy
+++ Forge/src/test/groovy/cgltest/CodecTesting.groovy
@@ -1,10 +1,12 @@
 package cgltest
 
 import groovy.transform.CompileStatic
+import groovy.transform.TupleConstructor
 import io.github.lukebemish.groovyduvet.wrapper.minecraft.api.codec.CodecSerializable
 
 @CompileStatic
 @CodecSerializable
+@TupleConstructor
 class CodecTesting {
     String myProperty
     static void yes() {
