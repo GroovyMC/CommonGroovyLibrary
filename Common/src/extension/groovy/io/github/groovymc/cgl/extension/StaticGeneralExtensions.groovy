4d6e63e6708163b1ad306cb21631a3e7099f5683
Matyrobbrt <matyrobbrt@gmail.com>
2022-09-02 15:33:09 +0300

    Click events

--- Common/src/extension/groovy/io/github/groovymc/cgl/extension/StaticGeneralExtensions.groovy
+++ Common/src/extension/groovy/io/github/groovymc/cgl/extension/StaticGeneralExtensions.groovy
@@ -18,13 +18,16 @@
 package io.github.groovymc.cgl.extension
 
 import groovy.transform.CompileStatic
+import net.minecraft.network.chat.ClickEvent
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.crafting.Recipe
 import net.minecraft.world.item.crafting.RecipeType
 
+import java.nio.file.Path
+
 @CompileStatic
 class StaticGeneralExtensions {
-    static <T extends Recipe> RecipeType<T> simple(ResourceLocation name) {
+    static <T extends Recipe> RecipeType<T> simple(RecipeType self, ResourceLocation name) {
         final actualName = name.toString()
         return new RecipeType<T>() {
             @Override
@@ -33,4 +36,28 @@ class StaticGeneralExtensions {
             }
         }
     }
+
+    // region ClickEvents
+    static ClickEvent openUrl(ClickEvent self, String url) {
+        new ClickEvent(ClickEvent.Action.OPEN_URL, url)
+    }
+    static ClickEvent copyToClipboard(ClickEvent self, String text) {
+        new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, text)
+    }
+    static ClickEvent openFile(ClickEvent self, String file) {
+        new ClickEvent(ClickEvent.Action.OPEN_FILE, file)
+    }
+    static ClickEvent openFile(ClickEvent self, Path file) {
+        new ClickEvent(ClickEvent.Action.OPEN_FILE, file.toString())
+    }
+    static ClickEvent runCommand(ClickEvent self, String command) {
+        new ClickEvent(ClickEvent.Action.RUN_COMMAND, command)
+    }
+    static ClickEvent suggestCommand(ClickEvent self, String command) {
+        new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command)
+    }
+    static ClickEvent changePage(ClickEvent self, int pageNumber) {
+        new ClickEvent(ClickEvent.Action.CHANGE_PAGE, pageNumber.toString())
+    }
+    // endregion
 }
