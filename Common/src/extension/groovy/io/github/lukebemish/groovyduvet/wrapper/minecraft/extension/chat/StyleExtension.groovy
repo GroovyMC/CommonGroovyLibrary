10f5c0e46b7b83119d8c656a00b795e42ef5555a
Luke Bemish <lukebemish@gmail.com>
2022-10-14 22:30:09 -0500

    Update license headers

--- Common/src/extension/groovy/io/github/lukebemish/groovyduvet/wrapper/minecraft/extension/chat/StyleExtension.groovy
+++ Common/src/extension/groovy/io/github/lukebemish/groovyduvet/wrapper/minecraft/extension/chat/StyleExtension.groovy
@@ -2,6 +2,7 @@
  * Copyright (C) 2022 GroovyMC and contributors
  * SPDX-License-Identifier: LGPL-3.0-or-later
  */
+
 //file:noinspection unused
 
 package io.github.lukebemish.groovyduvet.wrapper.minecraft.extension.chat
--- Common/src/transform/groovy/io/github/groovymc/cgl/api/environment/Platform.groovy
+++ Common/src/transform/groovy/io/github/groovymc/cgl/api/environment/Platform.groovy
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2022 GroovyMC and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 package io.github.groovymc.cgl.api.environment
 
 enum Platform {
